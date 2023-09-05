6962c7818023af760f5c74b28401abdd48e5174c
DevenDanidhariya <123365201+DevenDanidhariya@users.noreply.github.com>
2023-09-05 07:20:42 +0530

    Delete prectice/test.java

--- prectice/test.java
+++
@@ -1,9 +0,0 @@
-
-class test{
-    public static void main(String[] args) {
-            Student s=new Student();
-
-            System.out.println(s.getstudentage());
-            System.out.println(s.getstudentname());
-    }
-}
